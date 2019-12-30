07f979d1ebfdd5a960c1580e07c2bbe64eb3880e
Joseph Chambers <joseph@michael-chambers.com>
2019-12-30 12:30:45 -0700

    adds IndexScreen adds navigation adds eslint

---
+++ src/screens/indexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => (
+  <View>
+    <Text>Index Screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
