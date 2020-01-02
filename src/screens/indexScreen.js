78600467a2c51d0be233f1577c804708fb87f91e
Joseph Chambers <joseph@michael-chambers.com>
2020-01-02 09:54:29 -0700

    adding context.

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
-const IndexScreen = () => (
-  <View>
-    <Text>Index Screen</Text>
-  </View>
-);
+const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => <Text>{item.title}</Text>}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({});
 
